22181c54f0a8faa50c8c7c2d50cc114563e0042a
Emman Mlmb <emmanjibi@gmail.com>
2025-05-22 22:14:41 +0100

    Create upload.js

---
+++ pages/api/upload.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import formidable from 'formidable';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const uploadDir = path.join(process.cwd(), '/uploads');
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  const form = new formidable.IncomingForm({
+    multiples: true,
+    uploadDir,
+    keepExtensions: true,
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error('Erreur de parsing :', err);
+      return res.status(500).json({ error: 'Erreur de parsing' });
+    }
+
+    return res.status(200).json({ message: 'Fichiers uploadés', files });
+  });
+}
